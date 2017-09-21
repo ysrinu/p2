0b72c4290acc350dbe254461ebd56f8292914a55
Srinivas Yelamanchili <y_srinu@yahoo.com>
2017-09-20 21:28:46 -0400

    Added php and css files

---
+++ calculateShippingCost.php
@@ -0,0 +1,44 @@
+<?php
+require('helpers.php');
+
+# Set default values for variables if not Set
+
+if (isset($_GET['custName'])) {
+  $custName=$_GET['custName'];
+}else {
+  $custName='';
+}
+
+# default ship type options (radio buttons) to null (unchecked)
+$ground="";
+$express="";
+$overnight="";
+$shipType="";
+
+if (isset($_GET['shipType']))  $shipType=$_GET['shipType'];
+
+# based on shipping type, set the corresponding radio button as checked
+if ($shipType=="ground")
+  $ground="CHECKED";
+elseif ($shipType=="express")
+  $express="CHECKED";
+elseif ($shipType=="overnight")
+  $overnight="CHECKED";
+
+$booksOnly="";
+# GET will not have booksOnly value (checkbox) if not set. The 2nd check in the if statmeent (to check if 'on'') is therefore redudant
+if (isset($_GET['booksOnly']) && $_GET['booksOnly']=="on") {
+  $booksOnly="CHECKED";
+}
+
+if (isset($_GET['fromZipCode'])) {
+  $fromZipCode=$_GET['fromZipCode'];
+}else {
+  $fromZipCode='';
+}
+
+if (isset($_GET['toZipCode'])) {
+  $toZipCode=$_GET['toZipCode'];
+}else {
+  $toZipCode='';
+}
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require('calculateShippingCost.php');
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Shipping Cost Calculator</title>
+</head>
+<body>
+    <section>
+    <header><h1>Shipping Cost Calculator</h1></header>
+    <p>
+        Calculate shipping cost between two different zip codes.
+    </p>
+    </section>
+
+    <form action="/" method="GET">
+        <fieldset>
+            <legend>Customer and Package Information</legend>
+            <label for="custName" class="category">Name (Required)</label><br />
+            <input type="text" name="custName" required id="custName" value="<?=sanitize($custName)?>" placeholder="Enter Customer Name" autofocus="autofocus"/><br /><br />
+            <label class="category">Shipping Type (Required)</label><br />
+            <input type="radio" name="shipType" required id="ground" value="ground" <?=sanitize($ground)?> /><label for="ground">Ground</label><br>
+            <input type="radio" name="shipType" required id="express" value="express" <?=sanitize($express)?> /><label for="express">Express</label><br>
+            <input type="radio" name="shipType" required id="overnight" value="overnight" <?=sanitize($overnight)?> /><label for="overnight">Overnight</label><br>
+            <br />
+            <input type="checkbox" name="booksOnly" id="booksOnly" <?=sanitize($booksOnly)?> /><label for="booksOnly">Books only</label><br />
+        </fieldset><br />
+        <fieldset>
+            <legend>Shipping Origin and Destination</legend>
+            <label for="fromZipCode" class="category">From ZIP Code (Required)</label><br />
+            <input type="text" pattern="[0-9]{5}" required name="fromZipCode" id="fromZipCode" value="<?=sanitize($fromZipCode)?>" placeholder="Enter 5-digit origin zip code" title="Enter 5-digit zipcode"/><br /><br />
+            <label for="toZipCode" class="category">To ZIP Code (Required)</label><br />
+            <input type="text" pattern="[0-9]{5}" required name="toZipCode" id="toZipCode" value="<?=sanitize($toZipCode)?>" placeholder="Enter 5-digit destination zip code" title="Enter 5-digit zipcode"/><br /><br />
+
+        </fieldset><br />
+        <input id="submitButton" type="submit" name="submitButton" value="Calculate Cost"/><br /><br />
+    </form>
+</body>
